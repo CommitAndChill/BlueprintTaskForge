2c74880ee1dfa4661a1181a949ffc913cb000e13
Saad K <saadrustam@gmail.com>
2025-07-16 21:48:00 -0700

    fix: fixed warnings - driving ShowNodeDescriptionWhilePlaying via a new user setting

--- Source/BlueprintTaskForge/Public/Settings/BtfRuntimeSettings.h
+++ Source/BlueprintTaskForge/Public/Settings/BtfRuntimeSettings.h
@@ -1,11 +1,11 @@
 ﻿// Copyright (c) 2025 BlueprintTaskForge Maintainers
-// 
+//
 // This file is part of the BlueprintTaskForge Plugin for Unreal Engine.
-// 
+//
 // Licensed under the BlueprintTaskForge Open Plugin License v1.0 (BTFPL-1.0).
 // You may obtain a copy of the license at:
 // https://github.com/CommitAndChill/BlueprintTaskForge/blob/main/LICENSE.md
-// 
+//
 // SPDX-License-Identifier: BTFPL-1.0
 
 #pragma once
@@ -26,6 +26,9 @@ public:
      * enforced in the "Exec Function" array? */
     UPROPERTY(Category = "Node Settings", EditAnywhere, Config)
     bool EnforceDeactivateExecFunction = true;
+
+    UPROPERTY(Category = "Node Settings", EditAnywhere, Config)
+    bool ShowNodeDescriptionWhilePlaying = false;
 
     virtual FName GetSectionName() const override;
     virtual FName GetCategoryName() const override;
